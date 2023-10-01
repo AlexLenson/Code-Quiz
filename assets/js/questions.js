f9d57f5f0bea0a1f3a9e53605805b1c512f0eccc
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-10-01 18:49:41 -0400

    added questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Which data type is used to represent whole numbers in JavaScript?",
+        choices: ["String", "Boolean", "Integer", "Number"],
+        answer: "Number"
+    },
+    {
+        title: "Which of the following is a valid comment in JavaScript?",
+        choices: ["// This is a comment", "<!--This is a comment-->", "/*This is a comment*/", "#This is a comment"],
+        answer: "// This is a comment"
+    },
+    {
+        title: "How do you write a function in JavaScript?",
+        choices: ["function: myFunction()", "function myFunction()", "myFunction(): function", "myFunction() = function"],
+        answer: "function myFunction()"
+    },
+    {
+        title: "What is the result of 2 + '2' in JavaScript?",
+        choices: ["4", "22", "TypeError", "NaN"],
+        answer: "22"
+    },
+    {
+        title: "Which built-in method can be used to convert a string to uppercase in JavaScript?",
+        choices: ["toUpperCase()", "toUpper()", "toUpperCaseCase()", "upperCase()"],
+        answer: "toUpperCase()"
+    }
+];
